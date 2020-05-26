3c841837317d2ce6462ac1802dc2dc811dc42f11
Edward Chau <edwardchau@berkeley.edu>
2020-05-26 03:17:00 -0700

    Ready?

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ store = []
 
 app.use(bodyParser.json())
 app.use(errorhandler())
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 
 app.get('/patients', (req, res) => {
     console.log("GET")
